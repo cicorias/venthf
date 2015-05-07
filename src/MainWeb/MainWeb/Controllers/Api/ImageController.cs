1ef554e1f54579a7f08f1f2c42ed983c194d68d5
Shawn Cicoria <github@cicoria.com>
2015-05-07 07:23:53 -0700

    fix path and module

--- src/MainWeb/MainWeb/Controllers/Api/ImageController.cs
+++ src/MainWeb/MainWeb/Controllers/Api/ImageController.cs
@@ -11,13 +11,17 @@ using System.Web.Hosting;
 
 namespace MainWeb.Controllers.Api
 {
+
+    [RoutePrefix("api/images")]
     public class ImageController : ApiController
     {
-        public HttpResponseMessage Get(string id)
+        
+        [Route("file/{name}")]
+        public HttpResponseMessage Get(string name)
         {
             var appData = HostingEnvironment.MapPath("~/App_Data");
 
-            string fileName = Path.Combine(appData, string.Format("{0}", id));
+            string fileName = Path.Combine(appData, string.Format("{0}", name));
 
             if (!File.Exists(fileName))
                 throw new HttpResponseException(HttpStatusCode.NotFound);
